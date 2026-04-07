31630ed698bf2be0a14fe4629560cf847cbf8829
AppSolves <appsolves@gmail.com>
2026-04-07 18:34:00 +0200

    fix: update bootstrap script URLs to remove unnecessary refs/heads segment

--- src/routes/install.ps1/+server.ts
+++ src/routes/install.ps1/+server.ts
@@ -2,7 +2,7 @@ import { redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 const BOOTSTRAP_PS1_URL =
-  "https://raw.githubusercontent.com/fidan-lang/fidan/refs/heads/main/scripts/bootstrap.ps1";
+  "https://raw.githubusercontent.com/fidan-lang/fidan/main/scripts/bootstrap.ps1";
 
 export const GET: RequestHandler = async () => {
   throw redirect(307, BOOTSTRAP_PS1_URL);
--- src/routes/install.sh/+server.ts
+++ src/routes/install.sh/+server.ts
@@ -2,7 +2,7 @@ import { redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 const BOOTSTRAP_SH_URL =
-  "https://raw.githubusercontent.com/fidan-lang/fidan/refs/heads/main/scripts/bootstrap.sh";
+  "https://raw.githubusercontent.com/fidan-lang/fidan/main/scripts/bootstrap.sh";
 
 export const GET: RequestHandler = async () => {
   throw redirect(307, BOOTSTRAP_SH_URL);
